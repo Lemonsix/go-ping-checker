796b8ef5f7bfe4eaa5f1da9c11958ef21b1fc484
Lemonsix <alan.g.asc@gmail.com>
2023-07-03 12:09:39 -0300

    funcionando

--- main.go
+++ main.go
@@ -13,19 +13,67 @@ type Domain struct {
 
 func ping(domain string, ch chan<- Domain) {
 	start := time.Now()
+
 	conn, err := net.Dial("ip4:icmp", domain)
 	if err != nil {
-		ch <- Domain{Name: domain, Ping: fmt.Sprintf("Error resolviendo la dirección IP %s: %s", domain, err.Error())}
-		return
-	}
-
-	if err != nil {
-		ch <- Domain{Name: domain, Ping: fmt.Sprintf("Error al establecer la conexión %s: %s", domain, err.Error())}
+		ch <- Domain{Name: domain, Ping: fmt.Sprintf("Error al crear la conexión ICMP: %s", err.Error())}
 		return
 	}
 	defer conn.Close()
+
+	// Configurar timeout de lectura
+	conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+
+	// Crear un mensaje ICMP Echo Request
+	icmpMsg := make([]byte, 8)
+	icmpMsg[0] = 8 // ICMP Echo Request Type
+	icmpMsg[1] = 0 // ICMP Echo Request Code
+	icmpMsg[2] = 0 // ICMP Echo Request Checksum (zeroed for now)
+	icmpMsg[3] = 0 // ICMP Echo Request Checksum (zeroed for now)
+	icmpMsg[4] = 0 // ICMP Echo Request Identifier (zeroed for now)
+	icmpMsg[5] = 0 // ICMP Echo Request Identifier (zeroed for now)
+	icmpMsg[6] = 0 // ICMP Echo Request Sequence Number (zeroed for now)
+	icmpMsg[7] = 0 // ICMP Echo Request Sequence Number (zeroed for now)
+
+	// Calcular el checksum para el mensaje ICMP
+	checksum := checkSum(icmpMsg)
+	icmpMsg[2] = byte(checksum >> 8)
+	icmpMsg[3] = byte(checksum)
+
+	// Enviar el mensaje ICMP al dominio
+	_, err = conn.Write(icmpMsg)
+	if err != nil {
+		ch <- Domain{Name: domain, Ping: fmt.Sprintf("Error al enviar el mensaje ICMP: %s", err.Error())}
+		return
+	}
+
+	// Esperar la respuesta ICMP
+	reply := make([]byte, 1500)
+	_, err = conn.Read(reply)
+	if err != nil {
+		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+			ch <- Domain{Name: domain, Ping: "Tiempo de espera agotado o superior a 2000ms"}
+		} else {
+			ch <- Domain{Name: domain, Ping: fmt.Sprintf("Error al leer la respuesta ICMP: %s", err.Error())}
+		}
+		return
+	}
+
+	// Calcular la duración de latencia
 	duration := time.Since(start)
 	ch <- Domain{Name: domain, Ping: duration.String()}
+}
+func checkSum(msg []byte) uint16 {
+	sum := 0
+	for i := 0; i < len(msg)-1; i += 2 {
+		sum += int(msg[i])*256 + int(msg[i+1])
+	}
+	if len(msg)%2 == 1 {
+		sum += int(msg[len(msg)-1]) * 256
+	}
+	sum = (sum >> 16) + (sum & 0xffff)
+	sum += sum >> 16
+	return uint16(^sum)
 }
 
 func orderByPing(domains []Domain) {
@@ -35,7 +83,6 @@ func orderByPing(domains []Domain) {
 		minIdx := i
 		for j := i + 1; j < n; j++ {
 			// Parsear la duración del ping
-
 			if domains[j].Ping < domains[minIdx].Ping {
 				minIdx = j
 			}
@@ -55,6 +102,8 @@ func main() {
 		{Name: "netflix.com"},
 		{Name: "facebook.com"},
 		{Name: "reddit.com"},
+		{Name: "reduno.com.ar"},
+		{Name: "radios.lu17.com"},
 		{Name: "office.com"},
 		{Name: "cnn.com"},
 		{Name: "imgur.com"},
